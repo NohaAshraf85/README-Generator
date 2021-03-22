0b8a4af2bb4f7dace63036da03be07e3e9d62918
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-03-22 01:57:05 -0400

    created the function that prompts the user with questions, next step is to do the research on how exactly i want the README.md file to include

--- index.js
+++ index.js
@@ -3,3 +3,39 @@ const fs = require("fs");
 const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+      },
+      {
+        type: "input",
+        name: "location",
+        message: "Where are you from?"
+      },
+      {
+        type: "input",
+        name: "hobby",
+        message: "What is your favorite hobby?"
+      },
+      {
+        type: "input",
+        name: "food",
+        message: "What is your favorite food?"
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Username"
+      },
+      {
+        type: "input",
+        name: "linkedin",
+        message: "Enter your LinkedIn URL."
+      }
+    ]);
+  }
+  
