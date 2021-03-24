14e8c87e67dc76d659bc8a91e5b291e9acd4bda7
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-03-24 11:59:23 -0400

    fixed the issue where the title was being displayed under the title, now the text entered from the terminal replaces the word title

--- index.js
+++ index.js
@@ -101,20 +101,26 @@ function promptUser() {
        
 
 
-    return `# Title 
-    ${answers.title}
+    return `# ${answers.title}
 ${licenseurl}
 
 ## Description 
     ${answers.description}
 ## Table of Contents 
 [Description](#description)
+
 [Installation](#installation)
+
 [Usage](#usage)
+
 [License](#license)
+
 [Contributing](#contributing)
+
 [Tests](#tests)
+
 [Questions](#questions)
+
 ## Installation 
     ${answers.installation}
 ## Usage 
