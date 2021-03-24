b426b8abbb503c44bcd1ee18e2ecb173788a3ee9
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-03-24 11:51:47 -0400

    updated the questions, added the licenses badges with their choices using if condition, added the e-mail and username to be included when the questions section is invoked, i also added a clickable link to the license sheild (badge) that appeares that takes the user to a website that shows the open souce license page

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function promptUser() {
         name: "description",
         message: "Please add the README description"
       },
-      {
-        type: "input",
-        name: "toc",
-        message: "Add the Table of Contents"
-      },
+      // {
+      //   type: "input",
+      //   name: "toc",
+      //   message: "Add the Table of Contents"
+      // },
       {
         type: "input",
         name: "installation",
@@ -40,9 +40,7 @@ function promptUser() {
           "GNU General Public License v3.0",
           "Apache License 2.0",
           "ISC License",
-          "Mozilla Public License 2.0",
-          "Boost Software License 1.0",
-          "The Unlicense"
+          "Mozilla Public License 2.0"
         ]
       },
       {
@@ -55,21 +53,68 @@ function promptUser() {
         name: "tests",
         message: "What type of testing?"
       },
+      // {
+      //   type: "input",
+      //   name: "questions",
+      //   message: "Questions here"
+      // },
       {
         type: "input",
-        name: "questions",
-        message: "Questions here"
+        name: "githubuser",
+        message: "Github User"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "email address"
       }
     ]);
   }
 
   function generateREADME(answers) {
+    var licenseurl="";
+
+    if(answers.license === "MIT License")
+    {
+      licenseurl="[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)";
+    }
+
+    else if(answers.license === "GNU General Public License v3.0")
+    {
+      licenseurl="[![License: GNU](https://img.shields.io/badge/License-GNU-green.svg)](https://opensource.org/licenses/gpl-license)";
+    }
+
+    else if(answers.license === "Apache License 2.0")
+    {
+      licenseurl="[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-green.svg)](https://opensource.org/licenses/Apache-2.0)";
+    }
+
+    else if(answers.license === "ISC License")
+    {
+      licenseurl="[![License: ISC](https://img.shields.io/badge/License-ISC-green.svg)](https://opensource.org/licenses/ISC)";
+    }
+
+    else if(answers.license === "Mozilla Public License 2.0")
+    {
+      licenseurl="[![License: GNU](https://img.shields.io/badge/License-Mozilla%20Public%20License%202.0-green.svg)](https://opensource.org/licenses/MPL-2.0)";
+    }
+       
+
+
     return `# Title 
     ${answers.title}
+${licenseurl}
+
 ## Description 
     ${answers.description}
 ## Table of Contents 
-    ${answers.toc}
+[Description](#description)
+[Installation](#installation)
+[Usage](#usage)
+[License](#license)
+[Contributing](#contributing)
+[Tests](#tests)
+[Questions](#questions)
 ## Installation 
     ${answers.installation}
 ## Usage 
@@ -80,8 +125,10 @@ function promptUser() {
     ${answers.contributing}
 ## Tests 
     ${answers.tests}
-## Questions 
-    ${answers.questions}`;
+## Questions
+For any questions, please contact me through [GitHub](github:${answers.githubuser}) 
+or [Email](email:${answers.email})`
+    ;
   }
 
   promptUser()
